44d1e3e09dfb4bcd4c36e432dc5d26e7bad17856
javdlgomez <100542213+javdlgomez@users.noreply.github.com>
2023-05-23 23:33:16 -0600

    Add files via upload

---
+++ Detección Geiger-Muller/builder/CMakeFiles/exampleLA02.exe.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleLA02.exe.dir/exampleLA02.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/exampleLA02.cc.o.d"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02ActionInitialization.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02ActionInitialization.cc.o.d"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02DetectorConstruction.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02DetectorConstruction.cc.o.d"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02EventAction.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02EventAction.cc.o.d"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02RunAction.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02RunAction.cc.o.d"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02SteppingAction.cc.o"
+  "CMakeFiles/exampleLA02.exe.dir/src/LA02SteppingAction.cc.o.d"
+  "exampleLA02.exe"
+  "exampleLA02.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleLA02.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
